443408aae5ef3413ca0944de9ca188efad832f0f
Sergey Hovhannisyan <sergey.hovhannisyan.1992@gmail.com>
2023-09-13 11:49:56 -0700

    changes

--- ProgrammingClass4.MvcLesson/Controllers/ProductTypeController1.cs
+++ ProgrammingClass4.MvcLesson/Controllers/ProductTypeController1.cs
@@ -4,6 +4,7 @@ namespace ProgrammingClass4.MvcLesson.Controllers
 {
     public class ProductTypeController1 : Controller
     {
+        public 
         public IActionResult Index()
         {
             return View();
