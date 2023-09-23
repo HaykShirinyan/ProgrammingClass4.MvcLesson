7acd3b9f74e5bffdd63b4c0fc18cd7ddedd41baf
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2023-09-23 13:22:47 -0700

    Revert "changes"
    This reverts commit 443408aae5ef3413ca0944de9ca188efad832f0f.

--- ProgrammingClass4.MvcLesson/Controllers/ProductTypeController1.cs
+++ ProgrammingClass4.MvcLesson/Controllers/ProductTypeController1.cs
@@ -4,7 +4,6 @@ namespace ProgrammingClass4.MvcLesson.Controllers
 {
     public class ProductTypeController1 : Controller
     {
-        public 
         public IActionResult Index()
         {
             return View();
