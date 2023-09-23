c1fddcab2e9cf754216c532eb956e59f090bbfc2
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2023-09-23 13:22:17 -0700

    Revert "chnages today"
    This reverts commit 7ca89bfa70234e4854b397bb632b7bf02749d659.

--- ProgrammingClass4.MvcLesson/Models/ProductTypes.cs
+++ ProgrammingClass4.MvcLesson/Models/ProductTypes.cs
@@ -8,10 +8,9 @@ namespace ProgrammingClass4.MvcLesson.Models
     {
         [Key]
         public int Id { get; set; }
-
         [Required]
         [StringLength(100)]
-        public required string Name { get; set; }
+        public string Name { get; set; }
 
         [StringLength(500)]
         public string Description { get; set; }
