adb6464e6ab43f4f192806ccee3b784445e85785
Sergey Hovhannisyan <sergey.hovhannisyan.1992@gmail.com>
2023-10-19 10:56:48 -0700

    changes

---
+++ ProgrammingClass4.MvcLesson/Controllers/ShoppingCartController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammingClass4.MvcLesson.Data;
+using ProgrammingClass4.MvcLesson.Models;
+
+namespace ProgrammingClass4.MvcLesson.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ShoppingCartController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCart = _dbContext.shoppingCarts.ToList();
+
+            return View(shoppingCart);
+        }
+    }
+}
