53f28e1b3aff8cbdf24b042819244315f40f3afb
Davit Avetisyan <avetisyandavit261413@gmail.com>
2023-10-22 20:34:48 +0400

    Updated product category view model

--- ProgrammingClass4.MvcLesson/ViewModels/ProductCategoryViewModel.cs
+++ ProgrammingClass4.MvcLesson/ViewModels/ProductCategoryViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using ProgrammingClass4.MvcLesson.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProgrammingClass4.MvcLesson.ViewModels
 {
     public class ProductCategoryViewModel
     {
+        [Required]
         public Product Product { get; set;}
         public List<Category>? Categories { get; set;}
         public List<ProductCategory>? ProductCategories {  get; set;}
